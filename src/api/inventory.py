7cdf4e2324f6fb355fc7d4515e0c08525e3df653
thuang-28 <tracymhuang@outlook.com>
2024-09-25 12:26:50 -0700

    Modify audit endpoint

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/inventory",
@@ -12,8 +14,10 @@ router = APIRouter(
 @router.get("/audit")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory WHERE id=1"))
+        row = result.fetchone()
+    return {"number_of_potions": row[1], "ml_in_barrels": row[2], "gold": row[3]}
 
 # Gets called once a day
 @router.post("/plan")
