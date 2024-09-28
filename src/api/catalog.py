5aee159e49dd34c47f78d78f5daf4d3071d777a1
thuang-28 <tracymhuang@outlook.com>
2024-09-27 22:37:19 -0700

    Check if stock exists

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,13 +15,14 @@ def get_catalog():
             sqlalchemy.text("SELECT num_green_potions FROM global_inventory")
         )
         available_stock = result.first().num_green_potions
-
-    return [
-        {
-            "sku": "GREEN_POTION",
-            "name": "green potion",
-            "quantity": available_stock,
-            "price": 50,
-            "potion_type": [0, 100, 0, 0],
-        }
-    ]
+    if available_stock:
+        return [
+            {
+                "sku": "GREEN_POTION",
+                "name": "green potion",
+                "quantity": available_stock,
+                "price": 50,
+                "potion_type": [0, 100, 0, 0],
+            }
+        ]
+    return []
