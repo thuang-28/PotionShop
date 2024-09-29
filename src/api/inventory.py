96fb9f6cf3ae2924894877f1c3c6ff79259baba2
thuang-28 <tracymhuang@outlook.com>
2024-09-28 17:41:48 -0700

    Sort imports

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -1,9 +1,11 @@
+import math
+
+import sqlalchemy
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from src.api import auth
-import math
-import sqlalchemy
+
 from src import database as db
+from src.api import auth
 
 router = APIRouter(
     prefix="/inventory",
