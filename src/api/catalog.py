847b715081af951403604f60763445399f54d4ce
thuang-28 <tracymhuang@outlook.com>
2024-10-07 17:37:35 -0700

    Correct incorrect regex group references

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -26,7 +26,7 @@ def get_catalog():
             .fetchall()
         )
     for potion in potions:
-        name = re.sub(r"([0-9]{1,3})([A-Z])_", r"$1% $2, ", potion["sku"]).replace(
+        name = re.sub(r"([0-9]{1,3})([A-Z])_", r"\g<1> \g<2>, ", potion["sku"]).replace(
             ", POTION", " Potion"
         )
         catalog.append(
