3fa79dcd482a73886977f91c7d227c3fa806eee3
thuang-28 <tracymhuang@outlook.com>
2024-11-14 00:34:31 -0800

    Shorten using mappings().all()

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -11,7 +11,7 @@ def get_catalog():
     Retrieves the catalog of items. Each unique item combination should have only a single price. You can have at most 6 potion SKUs offered in your catalog at one time.
     """
     with db.engine.begin() as connection:
-        potions = connection.execute(
+        catalog = connection.execute(
             sqlalchemy.text(
                 """
                       WITH st AS (
@@ -19,9 +19,11 @@ def get_catalog():
                              FROM potion_strategy
                             WHERE potion_strategy.day_of_week::text = TO_CHAR(now(), 'fmDay')
                           )
-                    SELECT potion_index.sku, price,
+                    SELECT potion_index.sku AS sku,
+                           REPLACE(INITCAP(potion_index.sku), '_', ' ') AS name,
                            COALESCE(SUM(qty_change), 0) AS quantity,
-                           ARRAY[red_pct, green_pct, blue_pct, dark_pct] AS type
+                           price,
+                           ARRAY[red_pct, green_pct, blue_pct, dark_pct] AS potion_type
                       FROM potion_index
                       JOIN potion_records ON potion_index.sku = potion_records.sku
                  LEFT JOIN st ON potion_index.sku = st.potion_sku
@@ -32,18 +34,6 @@ def get_catalog():
                      LIMIT 6
                     """
             )
-        ).all()
-    catalog = []
-    for potion in potions:
-        name = potion.sku.replace("_", " ").title()
-        catalog.append(
-            {
-                "sku": potion.sku,
-                "name": name,
-                "quantity": potion.quantity,
-                "price": potion.price,
-                "potion_type": potion.type,
-            }
-        )
+        ).mappings().all()
     print("[Log] Available Catalog:", catalog)
     return catalog
