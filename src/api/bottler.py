4f8f00cdb96ac9c3bf434aea11966a14f4a1399b
thuang-28 <tracymhuang@outlook.com>
2024-11-14 00:41:36 -0800

    Filter using having

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -81,6 +81,7 @@ def get_bottle_plan():
                       JOIN potion_strategy ON potion_strategy.potion_sku = potion_index.sku
                        AND potion_strategy.day_of_week::text = TO_CHAR(now(), 'fmDay')
                      GROUP BY potion_index.sku, favorability
+                     HAVING bottle_limit - COALESCE(SUM(qty_change), 0) > 0
                      ORDER BY favorability DESC, can_bottle DESC;
                     """
                 )
