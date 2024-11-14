8a582aba0b20679de303eb71095cd6b3c09df54d
thuang-28 <tracymhuang@outlook.com>
2024-11-13 22:13:11 -0800

    Set cap

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -120,7 +120,7 @@ def get_bottle_plan():
                 if potion.potion_type[i] != 0
             )
         )
-        num_mixable = min(max_qty, potions_left)
+        num_mixable = min(max_qty, potions_left, 50 - potion.quantity)
         if num_mixable > 0:
             for i in range(4):
                 ml_list[i] -= num_mixable * potion.potion_type[i]
